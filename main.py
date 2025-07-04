8aa64570b7133e626abf36187d817717b20fa333
rosma <rosmarygp11@gmail.com>
2025-07-04 14:08:45 +0200

    Decision Tree Classifier model created

---
+++ main.py
@@ -0,0 +1,37 @@
+import pandas as pd
+
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import classification_report
+
+data = pd.read_csv("adult.csv")
+data = data.dropna()
+data = data.drop(columns=["fnlwgt", "education.num", "capital.gain", "capital.loss"])
+
+
+label_encoders = {}
+
+for col in data.select_dtypes(include='object').columns:
+    le = LabelEncoder()
+    data[col] = le.fit_transform(data[col])
+    label_encoders[col] = le
+print(data.head())
+
+X = data.drop("income", axis = 1)
+y = data["income"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=21)
+
+model = DecisionTreeClassifier(max_depth=5, random_state=21)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+print(classification_report(y_test, y_pred))
+
+
+
+
+
+
+
