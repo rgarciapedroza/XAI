60e452af07306d26e5bc9251a5f6b6fa0a9a0676
rosma <rosmarygp11@gmail.com>
2025-07-07 13:57:35 +0200

    Model improved

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import pandas as pd
 
 from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV, train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
 
 import shap
 
@@ -19,15 +20,26 @@ for col in data.select_dtypes(include='object').columns:
     le = LabelEncoder()
     data[col] = le.fit_transform(data[col])
     label_encoders[col] = le
-print(data.head())
 
 X = data.drop("income", axis = 1)
 y = data["income"]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=21)
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+X_scaled_df = pd.DataFrame(X_scaled, columns=X.columns)
+X_train, X_test, y_train, y_test = train_test_split(X_scaled_df, y, test_size=0.2, random_state=21)
 
-model = LogisticRegression(max_iter=1000, random_state=21)
-model.fit(X_train, y_train)
+param_grid = {
+    'C': [0.01, 0.1, 1, 10],
+    'penalty': ['l1', 'l2'],
+    'solver': ['liblinear'],
+    'class_weight': ['balanced']
+}
+
+grid = GridSearchCV(LogisticRegression(max_iter=1000, random_state=21), param_grid, cv=5, scoring='accuracy')
+grid.fit(X_train, y_train)
+
+model = grid.best_estimator_
 
 y_pred = model.predict(X_test)
 print(classification_report(y_test, y_pred))
